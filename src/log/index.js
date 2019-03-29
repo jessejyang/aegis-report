187354a0b4fc7103d8a94d79caec99e206be5057
tickli <tickli@tencent.com>
2019-03-29 16:00:25 +0800

    feat: 离线日志兼容 sendBeacon

--- src/log/index.js
+++ src/log/index.js
@@ -101,43 +101,16 @@ export default class Log {
         }
     }
 
-    reportOffline ({ logs, msgObj, urlObj, startDate, endDate }) {
-        let iframe = document.createElement('iframe')
+    reportOffline (params) {
         const { id, uin, url } = this.config
         const { userAgent } = navigator
-        iframe.name = 'badjs_offline_' + (new Date() - 0)
-        iframe.frameborder = 0
-        iframe.height = 0
-        iframe.width = 0
-        iframe.src = 'javascript:false'
-        let data = JSON.stringify({ logs, msgObj, urlObj, userAgent, startDate, endDate, id, uin })
+
+        let data = JSON.stringify(Object.assign(params, { userAgent, id, uin }))
         if (this.config.deflate && window.pako) {
             data = encodeURIComponent(window.pako.deflate(data, { to: 'string' }))
         }
+        const _url = url + '/offlineLog'
 
-        iframe.onload = function () {
-            const form = document.createElement('form')
-            form.style.display = 'none'
-            form.target = iframe.name
-            form.method = 'POST'
-            form.action = url + '/offlineLog'
-            const input = document.createElement('input')
-            input.style.display = 'none'
-            input.type = 'hidden'
-            input.name = 'offline_log'
-            input.value = data
-
-            iframe.contentDocument.body.appendChild(form)
-            form.appendChild(input)
-            form.submit()
-            console.log('report offline log success')
-            setTimeout(function () {
-                document.body.removeChild(iframe)
-                iframe = null
-            }, 5000)
-
-            iframe.onload = null
-        }
-        document.body.appendChild(iframe)
+        send(_url, data, 'post')
     }
 }
--- src/report/index.js
+++ src/report/index.js
@@ -1,11 +1,55 @@
-export default function send (url, data) {
-    if (navigator.sendBeacon && typeof navigator.sendBeacon === 'function') {
-        try {
-            navigator.sendBeacon(url, data)
-        } catch (e) {
-            new Image().src = url
+function beaconPollyfill (url, data, type) {
+    if (type === 'post') {
+        let iframe = document.createElement('iframe')
+        iframe.name = 'badjs_offline_' + (new Date() - 0)
+        iframe.frameborder = 0
+        iframe.height = 0
+        iframe.width = 0
+        iframe.src = 'javascript:false'
+
+        iframe.onload = function () {
+            const form = document.createElement('form')
+            form.style.display = 'none'
+            form.target = iframe.name
+            form.method = 'POST'
+            form.action = url + '/offlineLog'
+            const input = document.createElement('input')
+            input.style.display = 'none'
+            input.type = 'hidden'
+            input.name = 'offline_log'
+            input.value = data
+
+            iframe.contentDocument.body.appendChild(form)
+            form.appendChild(input)
+            form.submit()
+            console.log('report offline log success')
+            setTimeout(function () {
+                document.body.removeChild(iframe)
+                iframe = null
+            }, 5000)
+
+            iframe.onload = null
         }
+        document.body.appendChild(iframe)
     } else {
         new Image().src = url
+    }
+}
+
+export default function send (url, data, type) {
+    if (navigator.sendBeacon && typeof navigator.sendBeacon === 'function') {
+        try {
+            if (type === 'post') {
+                const fd = new FormData()
+                fd.append('offline_log', data)
+                navigator.sendBeacon(url, fd)
+            } else {
+                navigator.sendBeacon(url, data)
+            }
+        } catch (e) {
+            beaconPollyfill(url, data, type)
+        }
+    } else {
+        beaconPollyfill(url, data, type)
     }
 }
