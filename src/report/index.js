a5c18bd9d4b5ee82a6ad98c8e1d5c7173b6815c8
tickli <lizhen55521@gmail.com>
2019-05-28 16:48:41 +0800

    fix: sendBeacon max size 65536

--- src/report/index.js
+++ src/report/index.js
@@ -1,41 +1,35 @@
 export function sendOffline (url, data) {
-    if (navigator.sendBeacon && typeof navigator.sendBeacon === 'function') {
-        const fd = new FormData()
-        fd.append('offline_log', data)
-        navigator.sendBeacon(url, fd)
-    } else {
-        let iframe = document.createElement('iframe')
-        iframe.name = 'badjs_offline_' + (new Date() - 0)
-        iframe.frameborder = 0
-        iframe.height = 0
-        iframe.width = 0
-        iframe.src = 'javascript:false'
+    let iframe = document.createElement('iframe')
+    iframe.name = 'badjs_offline_' + (new Date() - 0)
+    iframe.frameborder = 0
+    iframe.height = 0
+    iframe.width = 0
+    iframe.src = 'javascript:false'
 
-        iframe.onload = function () {
-            const form = document.createElement('form')
-            form.style.display = 'none'
-            form.target = iframe.name
-            form.method = 'POST'
-            form.action = url + '/offlineLog'
-            const input = document.createElement('input')
-            input.style.display = 'none'
-            input.type = 'hidden'
-            input.name = 'offline_log'
-            input.value = data
+    iframe.onload = function () {
+        const form = document.createElement('form')
+        form.style.display = 'none'
+        form.target = iframe.name
+        form.method = 'POST'
+        form.action = url + '/offlineLog'
+        const input = document.createElement('input')
+        input.style.display = 'none'
+        input.type = 'hidden'
+        input.name = 'offline_log'
+        input.value = data
 
-            iframe.contentDocument.body.appendChild(form)
-            form.appendChild(input)
-            form.submit()
-            console.log('report offline log success')
-            setTimeout(function () {
-                document.body.removeChild(iframe)
-                iframe = null
-            }, 5000)
+        iframe.contentDocument.body.appendChild(form)
+        form.appendChild(input)
+        form.submit()
+        console.log('report offline log success')
+        setTimeout(function () {
+            document.body.removeChild(iframe)
+            iframe = null
+        }, 5000)
 
-            iframe.onload = null
-        }
-        document.body.appendChild(iframe)
+        iframe.onload = null
     }
+    document.body.appendChild(iframe)
 }
 
 export default function send (url, data) {
